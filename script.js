88ab4c08ae58b086f3020d68e7e6db2f5cbeb395
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-09-21 12:13:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
-
+var x = document.getElementById("form1");
+	let name = "";
+	for(var i=0; i<x.length; i=i+1)
+		{
+			if(x.element[i].value != 'Submit')
+				name = name + x.element[i].value + " "
+		}
+	alert(name.substring(0, nname.length - 1));
 }
