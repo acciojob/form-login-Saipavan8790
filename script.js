12f5075ee2bfafc55e415e7d172ed09a28ba7646
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-09-21 12:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getFormvalue() {
     //Write your code here
-var x = document.getElementById("form1");
-	let name = "";
-	for(var i=0; i<x.length; i=i+1)
-		{
-			if(x.element[i].value != 'Submit')
-				name = name + x.element[i].value + " "
-		}
-	alert(name.substring(0, name.length - 1));
+var x=document.getElementById("form1");
+let name = ""
+for (var i=0;i<x.length;i++) {
+if (x.elements[i].value!='Submit')
+name += x.elements[i].value + " "
+}
+
+alert(name.substring(0,name.length-1));
 }
